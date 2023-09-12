e5b7bac9e8297d70240644668afde049cd1220be
acapone001 <49999555+acapone001@users.noreply.github.com>
2023-09-11 17:12:23 -0700

    Update streamlit_app.py
    add connection to snowflake
    add cursor and select statement to display information

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,13 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # outputs the normalized json text as a table
 streamlit.dataframe(fruityvice_normalized)
+
+# snowflake connection section
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(), current_region()")
+my_data_row = my_cur.fetchone()
+
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
